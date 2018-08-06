3427cff3c1044b7403ab6b8fdab4ffdad2a836f3
Xinchro <TheXrocks@gmail.com>
2018-08-06 19:23:25 +0100

    moved across to discord.js fully and did some fine tuning with how messages are sent back

--- index.js
+++ index.js
@@ -28,8 +28,9 @@ function getUptime() {
 }
 
 // when ready, print bot name
-client.on("ready", e => {
-  console.log("Connected as: " + client.user.tag)
+client.on("ready", () => {
+  console.log(`Connected as: ${client.user.username}#${client.user.discriminator}`)
+  client.user.setActivity("How To Be Human 101", { type: "LISTENING" })
 })
 
 client.on("message", msg => {
@@ -41,20 +42,39 @@ client.login(process.env.TOKEN)
 
 // check if message is private(DM) or public(server/guild)
 function dealWithMessage(message) {
-  if(message.channel.isPrivate) {
-    if(message.author.username.toLowerCase() != botname.toLowerCase()) {
-      dealWithDMMessage(message).then((data) => {
-        message.channel.send(data.msg, data.tts, data.embed)
-      }, (data) => { if(data != null) message.channel.send(data.msg, data.tts, data.embed) })
-    }
+  if(message.channel.type==="dm") {
+    dealWithDMMessage(message)
+      .then((data) => {
+        send(message, { content: data.content, options: data.options })
+      })
+      .catch((data) => {
+        // fail silently
+        logError(data)
+      })
   } else {
-    dealWithServerMessage(message).then((data) => {
-      if (data.msg === '' && data.embed) {
-        message.channel.sendEmbed(data.embed)
-      }
-      message.channel.send(data.msg)
-    }, (data) => { if(data != null) message.channel.send(data.msg, data.tts, data.embed) })
+    dealWithServerMessage(message)
+      .then((data) => {
+        send(message, { content: data.content, options: data.options })
+      })
+      .catch((data) => {
+        // fail silently
+        logError(data)
+      })
   }
+}
+
+function send(srcMessage, message) {
+  if(srcMessage.author.id === process.env.BOTID) return
+  if(!message.content) return
+
+  srcMessage.channel.send(message.content, message.options)
+    .then()
+    .catch(console.error)
+}
+
+function logError(error) {
+  if(error === null) return // ignore null errors(nothing to do)
+  console.error(error)
 }
 
 // deals with messages sent in a DM
@@ -72,8 +92,15 @@ function dealWithDMMessage(message) {
       resolve({ msg:"oh" })
       reject(null)
     } else {
-      message.channel.uploadFile(fs.readFileSync("assets/img/shoo.gif"), "assets/img/shoo.gif")
-      .then(resolve({ msg: "Don't DM me. Go away." }))
+      resolve({
+        content: "Don\"t DM me. Go away.",
+        obj: {
+          files: [{
+            attachment: "assets/img/shoo.gif",
+            name: "shoo.gif"
+          }]
+        }
+      })
     }
   })
 }
@@ -84,17 +111,14 @@ function dealWithServerMessage(message) {
 
   // checks if the message is a command("!")
   if(msg[0] === "!") {
-    return checkCommand(msg.split("!")[1])
+    return checkCommand(msg.split("!")[1], message.channel)
   } else
   // checks if the message is @ the bot
   if(msg.split(" ")[0] === botclient) {
     return replyToPing(message)
   } else {
     return new Promise((resolve, reject) => {
-      switch(msg) {
-        default:
-          reject(null)
-      }
+      reject(null)
     })
   }
 }
@@ -105,10 +129,10 @@ function replyToPing(message) {
   return new Promise((resolve, reject) => {
     try {
       if(msgAfterAt.match(/(?:\<\@\!.*\>)/gi)) {
-        resolve({msg: `Hello ${message.author.username}, what about ${msgAfterAt.match(/(?:\<\@\!.*\>)/g)[0]}?`})
+        resolve({content: `Hello ${message.author.username}, what about ${msgAfterAt.match(/(?:\<\@\!.*\>)/g)[0]}?`})
       } else
       if(msgAfterAt.match(/^(?:\w)+ me/gi)) {
-        resolve({msg: "no"})
+        resolve({content: "no"})
       } else
       if(msgAfterAt === "hello"
         || msgAfterAt === "hello "
@@ -122,14 +146,14 @@ function replyToPing(message) {
         if(Math.floor((Math.random()*100)+1) < 90) {
           let negative = textToArray("assets/text/negative_replies.txt", "\n")
 
-          resolve({ msg: negative[Math.floor((Math.random()*negative.length))] })
+          resolve({ content: negative[Math.floor((Math.random()*negative.length))] })
         } else {
-          resolve({ msg: `Hello <@!${message.author.id}>!` })
+          resolve({ content: `Hello <@!${message.author.id}>!` })
         }
       } else
       if(msgAfterAt === "i love you"
         || msgAfterAt === "ily") {
-        resolve({msg: ":congratulations:"})
+        resolve({content: ":congratulations:"})
       } else
       if(msgAfterAt === "git gud"
         || msgAfterAt === "gitgud") {
@@ -153,7 +177,7 @@ function textToArray(file, del) {
 
 
 // checks and deals with commands
-function checkCommand(command) {
+function checkCommand(command, channel) {
   switch(command.split(" ")[0]) {
     case "hello":
       return hello()
@@ -164,12 +188,30 @@ function checkCommand(command) {
     case "delete":
       break
     case "uptime":
-      return new Promise((res, rej) => { res({ msg: getUptime()}); rej(null) })
+      return new Promise((res, rej) => {
+        res({ content: getUptime()})
+        rej("Failed to show uptime")
+      })
+      break
+    case "yay":
+      return new Promise((res, rej) => {
+          res({
+            content: {
+              files: [{
+                attachment: "assets/audio/yinbyay.flac",
+                name: "yay.flac"
+              }]
+            }
+          })
+        })
+      break
+    case "AAAAAaaaaAAAAhhhh":
+      return new Promise((res, rej) => { res({ content: "AAAAHHHHH! :scream:"}) })
       break
     case "overwatch":
       return getOverwatchStats(command.split(" ")[1], command.split(" ")[2], command.split(" ")[3])
-      .then((data) => { return formatOverwatchStats(data) })
-      .then((data) => { return data })
+        .then((data) => { return formatOverwatchStats(data) })
+        .then((data) => { return data })
       break
     default:
       return new Promise((resolve, reject) => reject(null))
@@ -181,20 +223,23 @@ function status() {
   return new Promise((resolve, reject) => {
     try {
       resolve({
-        msg: "", tts: false, embed: {
-          color: 0x00FF00,
-          title: "Running smoothly!",
-          fields: [
-            { name: "Stats", value: `Uptime: ${getUptime()}`, inline: false }
-          ]
+        content: {
+          embed: {
+            color: 0x00FF00,
+            title: "Running smoothly!",
+            fields: [
+              { name: "Stats", value: `Uptime: ${getUptime()}`, inline: false }
+            ]
+          }
         }
       })
     } catch(err) {
-      console.error(err)
       reject({
-        msg: "", tts: false, embed: {
-          color: 0xFF0000,
-          title: "This is fine. :fire:"
+        content: {
+          embed: {
+            color: 0xFF0000,
+            title: "This is fine. :fire:"
+          }
         }
       })
     }
@@ -207,20 +252,21 @@ function hello() {
   return new Promise((resolve, reject) => {
     try {
       resolve({
-        msg: "", tts: false, embed: {
-          color: 0x00FF00,
-          author: { name: botname },
-          url: "https://xinchronize.com",
-          fields: [
-            { name: "Hello!", value: `I am ${botname}! Beep boop!`, inline: false },
-            { name: "Creator", value: "I was created by Saltchr-, I mean Xinchro!", inline: true },
-            { name: "Stats", value: "I was last updated at some point\nI have been awake for some time", inline: true }
-          ],
-          footer: { text: "Powered by salt.", icon_url: "https://xinchronize.com/assets/logo.png" }
+        content: {
+          embed: {
+            color: 0x00FF00,
+            author: { name: botname },
+            url: "https://xinchronize.com",
+            fields: [
+              { name: "Hello!", value: `I am ${botname}! Beep boop!`, inline: false },
+              { name: "Creator", value: "I was created by a͜҉n̕ ̶͜id̵̶i̴o̶t̴̢ ..... I mean Xinchro!", inline: true },
+              { name: "Stats", value: "I was last updated at̷ ̶͏́s̡̕͝o̡m̷̀́e̶͟ ̢po̸i̡̛n̛͡t͏\nI have been awake for l̷͟o̧͟͜n͝g͜͠ e̷no̢͜u̶g̷̡͡h", inline: true }
+            ],
+            footer: { text: "Powered by salt.", icon_url: "https://xinchronize.com/assets/logo.png" }
+          }
         }
       })
     } catch(err) {
-      console.error(err)
       reject(null)
     }
   })
@@ -235,38 +281,38 @@ function getOverwatchStats(user, plat, reg) {
       if(data.status === 200) {
         resolve(data.json())
       } else {
-        reject({ msg: "Failed to retrieve Overwatch data." })
+        reject({ content: "Failed to retrieve Overwatch data." })
       }
     },
-    (data) => { reject({ msg: "Failed to retrieve Overwatch data." }) }
+    (data) => { reject({ content: "Failed to retrieve Overwatch data." }) }
     )
   })
 }
 
 // formats the overwatch data and resolves with an embed
 function formatOverwatchStats(data) {
-  console.log(data)
   return new Promise((resolve, reject) => {
     try {
       resolve({
-        msg: "", tts: false, embed: {
-          color: 0x3498db,
-          author: { name: data.username, icon_url: data.portrait },
-          title: data.username,
-          url: `https://playoverwatch.com/en-us/career/pc/eu/${data.username}`,
-          thumbnail: { url: data.competitive.rank_img || "" },
-          fields: [
-            { name: "Current season SR", value: data.competitive.rank || 0, inline:false },
-            { name: "Competitive",
-              value: `${data.playtime.competitive || 0}\nWon: ${data.games.competitive.won || 0}\nLost: ${data.games.competitive.lost || 0}`, inline:true },
-            { name: "Quickplay",
-              value: `${data.playtime.quickplay || 0}\nWon: ${data.games.quickplay.won || 0}`, inline:true },
-          ],
-          footer: { text: "Data provided by https://github.com/alfg/overwatch-api" }
+        content: {
+          embed: {
+            color: 0x3498db,
+            author: { name: data.username, icon_url: data.portrait },
+            title: data.username,
+            url: `https://playoverwatch.com/en-us/career/pc/eu/${data.username}`,
+            thumbnail: { url: data.competitive.rank_img || "" },
+            fields: [
+              { name: "Current season SR", value: data.competitive.rank || 0, inline:false },
+              { name: "Competitive",
+                value: `${data.playtime.competitive || 0}\nWon: ${data.games.competitive.won || 0}\nLost: ${data.games.competitive.lost || 0}`, inline:true },
+              { name: "Quickplay",
+                value: `${data.playtime.quickplay || 0}\nWon: ${data.games.quickplay.won || 0}`, inline:true },
+            ],
+            footer: { text: "Data provided by https://github.com/alfg/overwatch-api" }
+          }
         }
       })
     } catch(err) {
-      console.error(err)
       reject(null)
     }
   })
