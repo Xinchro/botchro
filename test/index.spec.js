b4fed0b9ee9248c9fa0fe4d2595249c1bbf6a4ec
Xinchro <xinchro@xinchronize.com>
2018-08-08 18:44:09 +0100

    added chai as promised and some (commented) tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,14 +1,20 @@
 const chai = require('chai')
+const chaiAsPromised = require("chai-as-promised")
 const path = require('path')
 const botchro = require('../index.js')
 
 const assert = chai.assert
+chai.use(chaiAsPromised);
 
 describe('Botchro', () => {
   describe('! commands', () => {
     describe('uptime', () => {
-      it('should be a promise', () => {
+      it('should be a promise, via function', () => {
         assert.typeOf(botchro.checkCommand("uptime"), 'promise')
+      })
+      it('should return a 0s uptime, via command', () => {
+        // assert.eventually.have.property(botchro.checkCommand("uptime"), 'content')
+        // assert.to.eventually.equal(botchro.checkCommand("uptime"), '0d0h0m0s')
       })
       it('should return a 0s uptime', () => {
         assert.equal(botchro.getUptime(), '0d0h0m0s')
