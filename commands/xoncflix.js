cb1bf2c2c7db5dcfb66a76b40633241effe8b2a5
Walter Reis <9714275+Xinchro@users.noreply.github.com>
2023-02-18 22:09:54 +0000

    added attendees to xoncflix embed

--- commands/xoncflix.js
+++ commands/xoncflix.js
@@ -1,57 +1,46 @@
-const { timeDifference } = require('../utils')
-
 module.exports.getEvents = async function (guild) {
   let eventsEmbed = {
     color: 0x0099ff,
     title: 'Xoncflix™ Entertainment Events',
     footer: { text: "Powered by salt.", icon_url: "https://xinchronize.com/assets/logo.png" }
   }
-  return guild.scheduledEvents.fetch().then(
-    events => {
-      let xoncflixEvents = events.filter(event => event.name.toLowerCase().startsWith('xoncflix'))
-      let timeTmEvents = events.filter(event => event.name.toLowerCase().startsWith('time™'))
+  const events = await guild.scheduledEvents.fetch()
 
-      let eventFields = []
 
-      eventFields.push(...xoncflixEvents.map(event => {
-        let fieldValue = ''
-        fieldValue += event.description ? `${event.description}\n` : ''
-        fieldValue += event.scheduledStartTimestamp ? `\n<t:${parseInt(event.scheduledStartTimestamp/1000)}:f>\n` : ''
-        fieldValue += event.scheduledStartTimestamp ? `\n<t:${parseInt(event.scheduledStartTimestamp/1000)}:R>\n` : ''
+  let filteredEvents = events.filter(event => event.name.toLowerCase().startsWith('xoncflix'))
+  events.filter(event => event.name.toLowerCase().startsWith('time™')).forEach(event => filteredEvents.set(event.id, event))
 
-        return {
-          name: event.name,
-          value: fieldValue,
-          inline: true
-        }
-      }))
-
-      eventFields.push(...timeTmEvents.map(event => {
-        let fieldValue = ''
-        fieldValue += event.description ? `${event.description}\n` : ''
-        fieldValue += event.scheduledStartTimestamp ? `\n<t:${parseInt(event.scheduledStartTimestamp/1000)}:f>}\n` : ''
-        fieldValue += event.scheduledStartTimestamp ? `\n<t:${parseInt(event.scheduledStartTimestamp/1000)}:R>\n` : ''
-
-        return {
-          name: event.name,
-          value: fieldValue,
-          inline: true
-        }
-      }))
-
-      if (xoncflixEvents.size + timeTmEvents.size === 0) {
-        return {
-          ...eventsEmbed,
-          title: 'Xoncflix™ Entertainment Events - Currently Closed',
-          description: 'There are no events scheduled. Very sad.'
-        }
-      }
-
-      return {
-        ...eventsEmbed,
-        description: `There are ${xoncflixEvents.size + timeTmEvents.size} event(s) scheduled.`,
-        fields: [...eventFields]
-      }
+  if (filteredEvents.size === 0) {
+    return {
+      ...eventsEmbed,
+      title: 'Xoncflix™ Entertainment Events - Currently Closed',
+      description: 'There are no events scheduled. Very sad.'
     }
-  )
+  }
+
+  let fields = await Promise.all(filteredEvents.map(async event => {
+    let fieldValue = ''
+    fieldValue += event.description ? `${event.description}\n` : ''
+    fieldValue += event.scheduledStartTimestamp ? `\n<t:${parseInt(event.scheduledStartTimestamp/1000)}:f>\n` : ''
+    fieldValue += event.scheduledStartTimestamp ? `\n<t:${parseInt(event.scheduledStartTimestamp/1000)}:R>\n` : ''
+
+    let users = Array.from(await event.fetchSubscribers()) // Map to array to get only usernames easier
+
+    fieldValue += `\nAttending: ${event.userCount}
+    ${users.map(userObj => {
+       return `${userObj[1].user.username}`
+    }).join(', ')}`
+
+    return {
+      name: event.name,
+      value: fieldValue,
+      inline: true
+    }
+  }))
+
+  return {
+    ...eventsEmbed,
+    description: `There are ${filteredEvents.size} event(s) scheduled.`,
+    fields
+  }
 }
